cbdb83d809fa48313c2f02c16f06c1f1b5bb8ca8
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2023-10-31 12:20:46 +0600

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -163,11 +163,10 @@
     var aximo_project_slider2 = new Swiper(".aximo-project-slider2", {
       // Optional parameters
       spaceBetween: 24,
-      direction: 'horizontal',
-      mousewheel: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+        clickable: false
       },
       // Responsive breakpoints
       breakpoints: {
@@ -181,6 +180,40 @@
           slidesPerView: 3
         }
       }
+    });
+    var $swiperSelector = $('.swiper-container');
+    $swiperSelector.each(function (index) {
+      var $this = $(this);
+      $this.addClass('swiper-slider-' + index);
+      var dragSize = $this.data('drag-size') ? $this.data('drag-size') : 50;
+      var freeMode = $this.data('free-mode') ? $this.data('free-mode') : false;
+      var loop = $this.data('loop') ? $this.data('loop') : false;
+      var slidesDesktop = $this.data('slides-desktop') ? $this.data('slides-desktop') : 4;
+      var slidesTablet = $this.data('slides-tablet') ? $this.data('slides-tablet') : 3;
+      var slidesMobile = $this.data('slides-mobile') ? $this.data('slides-mobile') : 2.5;
+      var spaceBetween = $this.data('space-between') ? $this.data('space-between') : 20;
+      var swiper = new Swiper('.swiper-slider-' + index, {
+        direction: 'horizontal',
+        loop: loop,
+        freeMode: freeMode,
+        spaceBetween: spaceBetween,
+        breakpoints: {
+          1920: {
+            slidesPerView: slidesDesktop
+          },
+          992: {
+            slidesPerView: slidesTablet
+          },
+          320: {
+            slidesPerView: slidesMobile
+          }
+        },
+        scrollbar: {
+          el: '.swiper-scrollbar',
+          draggable: true,
+          dragSize: dragSize
+        }
+      });
     });
 
     /*--------------------------------------------------------------
