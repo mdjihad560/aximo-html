d58bc6e6c6369b1996a401b93d9880670e1c9db5
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2023-11-03 10:42:10 +0600

    js update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 (function ($) {
   "use strict";
 
@@ -32,6 +36,7 @@
     return this.length;
   };
   $(function () {
+    var _Swiper;
     /*--------------------------------------------------------------
     fugu PRELOADER JS INIT
     --------------------------------------------------------------*/
@@ -145,7 +150,7 @@
     /*--------------------------------------------------------------
     fugu PRICING TABLE JS INIT
     ------------------------------------------------------------*/
-    var aximo_project_slider = new Swiper(".aximo-project-slider", {
+    var aximo_project_slider = new Swiper(".aximo-project-slider", (_Swiper = {
       // Optional parameters
       spaceBetween: 24,
       direction: 'horizontal',
@@ -153,20 +158,20 @@
       pagination: {
         el: ".swiper-pagination",
         clickable: true
-      },
-      // Responsive breakpoints
-      breakpoints: {
-        640: {
-          slidesPerView: 1
-        },
-        900: {
-          slidesPerView: 2
-        },
-        1600: {
-          slidesPerView: 3.5
-        }
       }
-    });
+    }, _defineProperty(_Swiper, "mousewheel", {
+      releaseOnEdges: true
+    }), _defineProperty(_Swiper, "breakpoints", {
+      640: {
+        slidesPerView: 1
+      },
+      900: {
+        slidesPerView: 2
+      },
+      1600: {
+        slidesPerView: 3.5
+      }
+    }), _Swiper));
 
     /*--------------------------------------------------------------
     fugu PRICING TABLE JS INIT
